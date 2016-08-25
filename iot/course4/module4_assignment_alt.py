5b29fe9f529ad510fb035b81fdffd2aefa2dda79
Jeroen <mail@jeroentoelen.com>
2016-08-26 00:13:05 +0200

    Add alternative for 'IoT / course4 / module4' assigment.

---
+++ iot/course4/module4_assignment_alt.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(13,GPIO.OUT)
+GPIO.setup(21,GPIO.IN)
+
+
+
+while True:
+	 input = GPIO.input(21)
+	 if input == True:
+	        GPIO.output(13, True)
+        	time.sleep(0.1)
+        	GPIO.output(13, False)
+        	time.sleep(0.1)
+	 elif input == False:
+		GPIO.output(13, True)
+		time.sleep(0.1)
